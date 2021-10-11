9d882f5df84c44fe236b4288ee18061966e057f9
Meonix <duymark42@gmail.com>
2021-10-11 20:05:31 +0700

    create project

---
+++ onlineSelling/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace onlineSelling.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string name { get; set; }
+    }
+}
---
+++ onlineSelling/ProductsData/ProductInterface.cs
@@ -0,0 +1,19 @@
+﻿using onlineSelling.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace onlineSelling.ProductsData
+{
+    interface ProductInterface
+    {
+        List<Product> getProducts();
+
+        Product getProduct(Guid id);
+
+        Product addProduct(Product product);
+
+        void deleteProduct(Guid id);
+    }
+}
